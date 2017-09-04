1f3ff29300390d45c57b858a1aa5242f8f836b68
应彧刚 <yingyugang@yingyugangdeMacBook-Pro.local>
2017-09-04 14:30:58 +0900

    xcode build.

---
+++ Assets/Scripts/XCodeBuild/Editor/XCodeBuild.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor.Callbacks;
+using UnityEditor;
+using UnityEditor.iOS.Xcode;
+using System.IO;
+
+public class XCodeBuild  {
+	[PostProcessBuild]
+	public static void OnPostprocessBuild (BuildTarget buildTarget, string path)
+	{
+		//if (buildTarget != BuildTarget.iPhone) { // For Unity < 5
+		if (buildTarget != BuildTarget.iOS) {
+			Debug.LogWarning ("Target is not iOS. AdColonyPostProcess will not run");
+			return;
+		}
+		string projPath = path + "/Unity-iPhone.xcodeproj/project.pbxproj";
+		PBXProject proj = new PBXProject ();
+		proj.ReadFromString (File.ReadAllText (projPath));
+		string target = proj.TargetGuidByName ("Unity-iPhone");
+		proj.SetBuildProperty (target, "ENABLE_BITCODE", "NO");
+		File.WriteAllText (projPath, proj.WriteToString ());
+	}
+}
